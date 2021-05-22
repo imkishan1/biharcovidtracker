d9166feace45aea0a808132db6c2aaf9ef76b1b8
Kisalaya Kishan <kislay.kishan007@gmail.com>
2021-05-22 13:52:05 +0530

    Glitch Fixed

--- JS/cowinapi.js
+++ JS/cowinapi.js
@@ -1,99 +1,96 @@
-function getstatus()
-{   
-
-    var date = document.querySelector('#date').value;
-    var pincode = document.querySelector("#pin").value;
-       async function getcowinapidata(a,b){
-        // const url ='https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode='+a+'&date='+b;
-        // https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=110001&date=31-03-2021
-        const url ='https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode='+a+'&date='+b;
-        const jsonurl = await fetch(url);
-        const getdata = await jsonurl.json();
-        const size = Object.keys(getdata.centers).length;
-        const cardul = document.getElementById('ulid');
-        var text = "Sorry, No Vaccination center is available for booking.";
-        if(size!=0)
-        {
-        for(var i =0;i<size;i++)
-        {
-         
-            var card = `  <li class="centerdetails">
+function getstatus() {
+  var date = document.querySelector("#date").value;
+  var pincode = document.querySelector("#pin").value;
+  async function getcowinapidata(a, b) {
+    // const url ='https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode='+a+'&date='+b;
+    // https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=110001&date=31-03-2021
+    const url =
+      "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=" +
+      a +
+      "&date=" +
+      b;
+    const jsonurl = await fetch(url);
+    const getdata = await jsonurl.json();
+    const size = Object.keys(getdata.centers).length;
+    const cardul = document.getElementById("ulid");
+    
+    var text = "Sorry, No Vaccination center is available for booking.";
+    if (size != 0) {
+      for (var i = 0; i < size; i++) {
+        var card = `  <li class="centerdetails">
             <div class="result-element">
               <div class="hospitalname">
                 <h5 id="name">${getdata.centers[i].name}</h5>
                 <div id="add" class="hospital-address">
                   ${getdata.centers[i].address}
+                  <p>${getdata.centers[i].sessions[0].vaccine}</p> 
                 </div>
               </div>
               <div class="slotsavail">
+              <div class="circle">
+              <h6>${getdata.centers[i].sessions[0].min_age_limit + "+"}</h6>
+            </div>
                 <h5>Slots Available</h5>
-                <h6 id="slots">${getdata.centers[i].sessions[0].available_capacity}</h6>
+                <h6 id="slots">${
+                  getdata.centers[i].sessions[0].available_capacity
+                }</h6>
               </div>
             </div>
           </li>
-          <div class="circle">
-          <h6>${getdata.centers[i].sessions[0].min_age_limit+'+'}</h6>
-        </div>`
-          cardul.innerHTML +=card;
-
-        }
-      }
-      else{
-        var card = `<div class="notfound"><p class="notfoundtxt">${text}</p></div>`
-        cardul.innerHTML+=card;
-       
-      }
-    
+        `;
+        cardul.innerHTML += card;
         
+      }
+    } else {
+      var card = `<div class="notfound"><p class="notfoundtxt">${text}</p></div>`;
+      cardul.innerHTML += card;
     }
-    getcowinapidata(pincode,date);
+  }
+  getcowinapidata(pincode, date);
 }
 
-function downloadfile(){
-  var refid = document.querySelector('#refid').value;
-  async function downloadodf(a)
-  {
-    const url = 'https://cdn-api.co-vin.in/api/v2/registration/certificate/public/download?beneficiary_reference_id='+a;
+function downloadfile() {
+  var refid = document.querySelector("#refid").value;
+  async function downloadodf(a) {
+    const url =
+      "https://cdn-api.co-vin.in/api/v2/registration/certificate/public/download?beneficiary_reference_id=" +
+      a;
     // console.log(url);
     // const datajs = await fetch(url);
     // const datajson = await datajs.json();
   }
- downloadodf(refid);
-
+  downloadodf(refid);
 }
 
-
 function SetMinDate() {
-    var now = new Date();
-    var day = parseInt(("0" + now.getDate()).slice(-2));
-    day = day;
-    day = day.toString();
-    var month = ("0" + (now.getMonth() + 1)).slice(-2);
-    var year = now.getFullYear();
-    var today = (day) + "-" + (month) + "-" + (year);
-    $('#date').val(today);
-    $('#date').attr('min', today);
+  var now = new Date();
+  var day = parseInt(("0" + now.getDate()).slice(-2));
+  day = day;
+  day = day.toString();
+  var month = ("0" + (now.getMonth() + 1)).slice(-2);
+  var year = now.getFullYear();
+  var today = day + "-" + month + "-" + year;
+  $("#date").val(today);
+  $("#date").attr("min", today);
 }
 SetMinDate();
 
-var date = document.getElementById('date').value;
+var date = document.getElementById("date").value;
 var pincode = document.getElementById("pin").value;
 
-document.getElementById('pin').addEventListener('blur',validateZip)
+document.getElementById("pin").addEventListener("blur", validateZip);
 
-function validateZip(){
-  const zip = document.getElementById('pin');
-  const re =/^[0-9]{6}(-[0-9])?$/
+function validateZip() {
+  const zip = document.getElementById("pin");
+  const re = /^[0-9]{6}(-[0-9])?$/;
 
-  if(!re.test(zip.value)){
-      pin.classList.add('is-invalid')
-      pin.classList.remove('is-valid')
-  }
-  else{
-      pin.classList.add('is-valid')
-      pin.classList.remove('is-invalid')
+  if (!re.test(zip.value)) {
+    pin.classList.add("is-invalid");
+    pin.classList.remove("is-valid");
+  } else {
+    pin.classList.add("is-valid");
+    pin.classList.remove("is-invalid");
   }
 }
-
 
 
